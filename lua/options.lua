731efdee619e59370b7dae70996f40d429a56b48
Henrique Augusto Figueiredo <henriqueaffv@gmail.com>
2025-10-09 23:23:33 -0300

    added more features for lsps

--- lua/options.lua
+++ lua/options.lua
@@ -20,7 +20,40 @@ vim.opt.virtualedit = "block" --only in visual block mode, virtual editing is en
 vim.opt.ignorecase = true --recognize commands even in lowercase (for search only)
 
 --about keybinds
+
+--keybinds already in use:
+-->>treesitter
+-- "<Leader>ss"
+-- "<Leader>si"
+-- "<Leader>sd"
+-- "<Leader>sc"
+-->>lsp
+--"<Leader>go"
+--"<Leader>ii"
+-->>other
+--"<Leader>ch"
+--"<A-down>"
+--"<A-up>"
+--"<A-j>"
+--"<A-k>"
+
+-->>Leader:
 vim.g.mapleader = "ç"
+-->>lsp
+vim.api.nvim_set_keymap("n", "<Leader>go", ":lua vim.lsp.buf.definition()<Enter>", {noremap = false})
+vim.api.nvim_set_keymap("n", "<Leader>ii", ":lua vim.lsp.buf.hover()<Enter>", {noremap = false})
+-->>other
+vim.api.nvim_set_keymap("n", "<Leader>ch", ":noh<Enter>", {noremap = false})
+vim.api.nvim_set_keymap("n", "<A-down>", "ddp", {noremap = false})
+vim.api.nvim_set_keymap("n", "<A-up>", "dd2kp", {noremap = false})
+vim.api.nvim_set_keymap("n", "<A-j>", "ddp", {noremap = false})
+vim.api.nvim_set_keymap("n", "<A-k>", "dd2kp", {noremap = false})
+
+--about diagnostics
+vim.diagnostic.config({
+    virtual_text = false, --display text at the rigt of the screen for diagnostics
+    virtual_lines = true --display text at the bottom of the lines for diagnostics
+})
 
 --about appearence
 vim.opt.termguicolors = true --allows more colors in the terminal
